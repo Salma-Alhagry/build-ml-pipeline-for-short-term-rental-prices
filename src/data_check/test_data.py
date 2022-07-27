68005848ba6d336316a6c1dde93239f6dade1bf1
Salma-Alhagry <salma.hagry@cu.edu.eg>
2022-07-27 14:40:46 +0200

    add docstring to 2 functions

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -60,9 +60,15 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
     assert scipy.stats.entropy(dist1, dist2, base=2) < kl_threshold
 
 def test_row_count(data):
+    """
+    Test number of data instances, it should be between 15000 and 1000000
+    """
     assert 15000 < data.shape[0] < 1000000
 
     
 def test_price_range(data, min_price, max_price):
+    """
+    Test the price to be in range of min_price and max_price
+    """
     assert data['price'].between(min_price, max_price).all()
                             
